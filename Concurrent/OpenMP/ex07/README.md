ac6c7d8fe53feb85c51003a681b0b11ab65c943b
GersonFeDutra <gerson.creative.des@gmail.com>
2024-02-09 19:30:21 -0300

    Concurrent: add ex08 OPENMP

---
+++ Concurrent/OpenMP/ex07/README.md
@@ -0,0 +1,3 @@
+Escreva um programa, com OpenMP, que dado um conjunto aleatório de dados (floats), calcule o desvio padrão deste conjunto de valores.
+
+<img src="./atv1.png">
---
+++ Concurrent/OpenMP/ex08/main.c
@@ -0,0 +1,7 @@
+/*
+Escreva um programa com OpenMP que ache e multiplique todos os elementos de um vetor por um escalar e depois achem o maior elemento. Primeiro fazem a multiplicação e depois acham o maior.
+*/
+int main(void)
+{
+
+}
